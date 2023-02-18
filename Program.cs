bc11355ff7fa0248b3a5ec822dc951df99fb65b1
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-02-18 16:18:28 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WizzyLiConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
